e5a612bb3055be6b5707088bed1150d41eef252f
Nikita Omelchenko <nikomel@Nikitas-MBP.attlocal.net>
2022-04-01 11:05:10 -0700

    ci: updating variabls

--- scripts/delete-old-stacks.sh
+++ scripts/delete-old-stacks.sh
@@ -2,17 +2,17 @@
 
 OLD_STACKS=$(aws cloudformation describe-stacks | jq -r '.Stacks[] | select(.StackName!="InitialStack" and .Parameters[].ParameterValue!="${CIRCLE_WORKFLOW_ID:0:7}").Parameters[].ParameterValue' | uniq)
 
-for stack in $OLD_STACKS
+for stackId in $OLD_STACKS
 do  
-    echo "Deleting stack with the id: ${stack}"
-    S3_BUCKET="s3://udapeople-$OldWorkflowID"
+    echo "Deleting stack with the id: ${stackId}"
+    S3_BUCKET="s3://udapeople-$stackId"
     if aws s3 ls $S3_BUCKET 2>&1 | grep -q 'An error occurred'
     then
-        echo "bucket does not exist."
+        echo "bucket with id $stackId does not exist."
     else
         aws s3 rm $S3_BUCKET --recursive
     fi
 
-    aws cloudformation delete-stack --stack-name udapeople-frontend-${OldWorkflowID}
-    aws cloudformation delete-stack --stack-name udapeople-backend-${OldWorkflowID}
+    aws cloudformation delete-stack --stack-name udapeople-frontend-${stackId}
+    aws cloudformation delete-stack --stack-name udapeople-backend-${stackId}
 done
