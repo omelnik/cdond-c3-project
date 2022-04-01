854e34d69e741bc742b34059171b4cb11c571c5f
Nikita Omelchenko <nikomel@Nikitas-MBP.attlocal.net>
2022-04-01 13:00:50 -0700

    ci: small change to the script

--- scripts/delete-old-stacks.sh
+++ scripts/delete-old-stacks.sh
@@ -8,7 +8,7 @@ echo "current stack: ${CURRENT_STACK_ID}"
 
 for stackId in $OLD_STACKS
 do
-    if [[ $stackId -ne $CURRENT_STACK_ID ]]; then
+    if [[ $stackId != $CURRENT_STACK_ID ]]; then
         echo "Deleting stack with the id: ${stackId}"
         S3_BUCKET="s3://udapeople-$stackId"
         if aws s3 ls $S3_BUCKET 2>&1 | grep -q 'An error occurred'; then
